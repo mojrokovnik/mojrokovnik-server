8cae260bde4a3985d98fa20ab7c5dbf27aeb330c
pavlovicnemanja <me@pavlovicnemanja.com>
2016-09-09 11:14:01 +0200

    initial commit

---
+++ src/AppBundle/Controller/DemoController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use FOS\RestBundle\Controller\FOSRestController;
+
+class DemoController extends FOSRestController {
+    public function getDemosAction() {
+        $data = array("hello" => "world");
+        $view = $this->view($data);
+        return $this->handleView($view);
+    }
+}
