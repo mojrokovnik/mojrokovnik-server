2320d2fc273a4861d2cc33f81188b1bcd6335e3c
pavlovicnemanja <me@pavlovicnemanja.com>
2016-10-17 15:03:01 +0200

    feat(registration): Added register functionality

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use FOS\RestBundle\Controller\FOSRestController;
+use Symfony\Component\HttpFoundation\Request;
 
 class UserController extends FOSRestController {
 
@@ -10,6 +11,20 @@ class UserController extends FOSRestController {
         $user = $this->get('security.token_storage')->getToken()->getUser();
 
         return $this->handleView($this->view($user));
+    }
+
+    public function postUserCreateAction(Request $request) {
+        $data = $request->request->all();
+
+        $manipulator = $this->get('fos_user.util.user_manipulator');
+        $manipulator->create($data['username'], $data['password'], $data['email'], null, null);
+
+        $view = array(
+            'status' => 200,
+            'message' => 'New user created!'
+        );
+
+        return $this->handleView($this->view($view));
     }
 
 }
